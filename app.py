4bcd5cbed2ed72e22ed51f42e147357fc03f94d7
dudi709 <dudi709@gmail.com>
2021-11-13 13:51:19 +0200

    Add the sqli model to the app file and make new table based on the model decision

--- app.py
+++ app.py
@@ -12,7 +12,20 @@ import dash_table
 
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from predict_sql_queries import build_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model, tokenizer1 = build_model()
+
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
@@ -24,29 +37,49 @@ colors = {
     "text": "#000000"
 }
 
-app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    dcc.Graph(id='Mygraph'),
-    html.Div(id='output-data-upload')
-])
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children=html.Img(src="https://img.icons8.com/color/96/000000/sql.png"), className="header-emoji"),
+                html.H1(
+                    children="SQL Query Analysis", className="header-title"
+                ),
+                html.P(
+                    children="Analyze sql queries and classify them"
+                             " according to queries that may contain"
+                             " malicious content (sql injection) and valid content.",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    # Allow multiple files to be uploaded
+                    multiple=True,
+                    className='upload-file'
+                ),
+            ]
+        ),
+        html.Div(
+            children=[
+                dcc.Graph(id='Mygraph')
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(id='output-data-upload')
+            ]
+        ),
+    ]
+)
 
 
 def parse_data(contents, filename):
@@ -58,6 +91,23 @@ def parse_data(contents, filename):
             # Assume that the user uploaded a CSV or TXT file
             df = pd.read_csv(
                 io.StringIO(decoded.decode('utf-8')))
+            # Add 'Type' column that classify each query to plain query and sqli query
+            query_type = []
+            for query in df.iloc[:, 0]:
+                temp_query_lst = []
+                temp_query_lst.append(query.lower())
+                token = tokenizer1.texts_to_sequences(temp_query_lst)
+                pad = pad_sequences(token, maxlen=150, padding="post")
+                decision = model.predict(pad)[0][0]
+
+                if decision < 0.6:
+                    query_type.append("plain")
+                    # print(f"{entry} - is not identified as a malicious query")
+                else:
+                    query_type.append("sqli")
+                    # print(f"{entry} - is identified as a malicious query !!!")
+            df['Type'] = query_type
+
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded))
@@ -108,12 +158,21 @@ def update_table(contents, filename):
         contents = contents[0]
         filename = filename[0]
         df = parse_data(contents, filename)
-
         table = html.Div([
             html.H5(filename),
             dash_table.DataTable(
                 data=df.to_dict('rows'),
-                columns=[{'name': i, 'id': i} for i in df.columns]
+                columns=[{'name': i, 'id': i} for i in df.columns],
+                style_header={
+                    'backgroundColor': 'rgb(30, 30, 30)',
+                    'color': 'white',
+                    'textAlign': 'left'
+                },
+                style_data={
+                    'backgroundColor': 'rgb(50, 50, 50)',
+                    'color': 'white',
+                    'textAlign': 'left'
+                },
             ),
             html.Hr(),
             html.Div('Raw Content'),
